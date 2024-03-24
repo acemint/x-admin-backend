23b39cd5c95ac9f60ff849a7488b4938de975f14
Steven Kristian <steven.kristian@gdn-commerce.com>
2024-03-25 02:58:05 +0700

    feat: added permission evaluator to check the validity of clinic access

--- src/main/java/com/clinic/xadmin/controller/constant/SecurityAuthorizationType.java
+++ src/main/java/com/clinic/xadmin/controller/constant/SecurityAuthorizationType.java
@@ -1,9 +1,16 @@
 package com.clinic.xadmin.controller.constant;
 
+import com.clinic.xadmin.constant.EmployeeRole;
+
 public class SecurityAuthorizationType {
 
   public static final String IS_FULLY_AUTHENTICATED = "isFullyAuthenticated()";
-  public static final String IS_DEVELOPER = "hasAnyRole('DEVELOPER')";
-  public static final String IS_ADMIN_OR_DEVELOPER = "hasAnyRole('DEVELOPER', 'ADMIN')";
+
+  public static final String HAS_PERMISSION_FUNCTION_PREFIX = "hasPermission(authentication, 'null', 'null', '";
+  public static final String ROLE_BASED_PERMISSION_PREFIX = "RoleBasedValidation::";
+  public static final String IS_DEVELOPER =
+      HAS_PERMISSION_FUNCTION_PREFIX + ROLE_BASED_PERMISSION_PREFIX + EmployeeRole.ROLE_DEVELOPER + "')";
+  public static final String IS_ADMIN_OR_DEVELOPER =
+      HAS_PERMISSION_FUNCTION_PREFIX + ROLE_BASED_PERMISSION_PREFIX + EmployeeRole.ROLE_DEVELOPER + "_" + EmployeeRole.ROLE_ADMIN + "')";
 
 }
