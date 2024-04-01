f4d703bad576173da567b8ee53b4bad6f0016e3a
Steven Kristian <steven.kristian@gdn-commerce.com>
2024-04-01 23:16:06 +0700

    refactor: added todo for sort parameters

--- src/main/java/com/clinic/xadmin/controller/employee/EmployeeControllerDefaultValue.java
+++ src/main/java/com/clinic/xadmin/controller/employee/EmployeeControllerDefaultValue.java
@@ -9,6 +9,7 @@ public interface EmployeeControllerDefaultValue {
   String DEFAULT_SORT_ORDER = "ASC";
   String DEFAULT_PAGE_NUMBER = "0";
   String DEFAULT_PAGE_SIZE = "10";
+  // TODO: Define where to put this sorted by, because this is somehow defined in controller but it is needed to be known in repository
   Set<String> AVAILABLE_SORTED_BY = EmployeeCustomRepositoryImpl.AVAILABLE_SORTED_BY;
 
 }
