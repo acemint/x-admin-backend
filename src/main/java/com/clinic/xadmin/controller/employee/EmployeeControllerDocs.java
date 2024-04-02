f3274642b29145b8b9654515d22e961de47e5aca
Steven Kristian <steven.kristian@gdn-commerce.com>
2024-04-02 22:10:18 +0700

    docs: added docs for employee controller

--- src/main/java/com/clinic/xadmin/controller/employee/EmployeeControllerDocs.java
+++ src/main/java/com/clinic/xadmin/controller/employee/EmployeeControllerDocs.java
@@ -15,6 +15,13 @@ public interface EmployeeControllerDocs {
 
   String GET_SELF_SUMMARY = "Get the data of current logged-in employee";
   String GET_EMPLOYEES_SUMMARY = "Get the list of employees in own's clinic";
-  String GET_EMPLOYEES_DESCRIPTION = "Returns list of employees in the clinic that the authenticated user is at";
+  String GET_EMPLOYEES_DESCRIPTION = "Returns list of employees in the clinic that the authenticated user is at."
+      + "The user of this API must at least be ADMIN.<br><br>"
+      + "Request Parameters: <br>"
+      + "name: to filter by `firstName` or `lastName`<br>"
+      + "sortBy: to sort the result in a certain way, support multiple sort fields<br>"
+      + "sortDirection: to sort the result in a given direction (1 sort direction will apply to all sort by fields)<br>"
+      + "pageNumber: returns the pageNumber, the index starts from 0<br>"
+      + "pageSize: the size of data requested per page";;
 
 }
