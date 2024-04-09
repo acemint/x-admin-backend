53759353590f98309c882c098f7daaba70fa7ecb
Steven Kristian <steven.kristian@gdn-commerce.com>
2024-04-09 16:45:54 +0700

    feat: added auditor aware for base entity

--- src/main/java/com/clinic/xadmin/entity/BaseEntity.java
+++ src/main/java/com/clinic/xadmin/entity/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.clinic.xadmin.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
@@ -10,7 +11,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -20,11 +27,24 @@ import java.util.UUID;
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
   @Id
   @Column(name = "id")
   private String id;
+
+  @CreatedBy
+  private String createdBy;
+
+  @LastModifiedBy
+  private String updatedBy;
+
+  @CreatedDate
+  private LocalDateTime createdDate;
+
+  @LastModifiedDate
+  private LocalDateTime updatedDate;
 
   @PrePersist
   protected void onCreate() {
