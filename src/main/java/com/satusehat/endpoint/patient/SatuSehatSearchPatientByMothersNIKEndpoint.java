12618040690c564f7dfecba3f202ef21ebbc7fa1
Steven Kristian <steven.kristian@gdn-commerce.com>
2024-04-15 22:04:29 +0700

    feat: added 2 endpoints satu sehat to get patient data

---
+++ src/main/java/com/satusehat/endpoint/patient/SatuSehatSearchPatientByMothersNIKEndpoint.java
@@ -0,0 +1,53 @@
+package com.satusehat.endpoint.patient;
+
+import com.satusehat.dto.response.StandardizedResourceResponse;
+import com.satusehat.dto.response.patient.PatientResourceResponse;
+import com.satusehat.endpoint.SatuSehatEndpoint;
+import com.satusehat.property.SatuSehatPropertyHolder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClient;
+
+import java.util.Map;
+
+public class SatuSehatSearchPatientByMothersNIKEndpoint implements
+    SatuSehatEndpoint<StandardizedResourceResponse<PatientResourceResponse>> {
+
+  private static final String PATH = "/Patient";
+  private static final String HTTP_METHOD = "GET";
+  private static final Map.Entry<String, String> AUTHORIZATION_HEADER = Map.entry("Authorization", "Bearer ");
+
+  private String authToken;
+  private final String nik;
+
+  public SatuSehatSearchPatientByMothersNIKEndpoint(String nik) {
+    this.nik = SatuSehatPropertyHolder.getInstance().getMotherNikUrl() + nik;
+  }
+
+  @Override
+  public ResponseEntity<StandardizedResourceResponse<PatientResourceResponse>> getMethodCall() {
+    RestClient restClient = RestClient.builder()
+        .baseUrl(SatuSehatPropertyHolder.getInstance().getBaseUrl())
+        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        .build();
+
+    return restClient.method(HttpMethod.valueOf(HTTP_METHOD))
+        .uri(uriBuilder -> uriBuilder
+            .path(PATH)
+            .queryParam("identifier", this.nik)
+            .build())
+        .header(AUTHORIZATION_HEADER.getKey(), AUTHORIZATION_HEADER.getValue() + this.authToken)
+        .retrieve()
+        .toEntity(new ParameterizedTypeReference<>() {});
+  }
+
+  @Override
+  public SatuSehatEndpoint<StandardizedResourceResponse<PatientResourceResponse>> setAuthToken(String authToken) {
+    this.authToken = authToken;
+    return this;
+  }
+
+}
